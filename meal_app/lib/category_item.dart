52422099314a025c0a0e708c2bec86214c19c578
Prashant Shrestha <sthprashant@gmail.com>
2020-01-05 12:55:01 +1100

    styling and themeing

--- meal_app/lib/category_item.dart
+++ meal_app/lib/category_item.dart
@@ -10,15 +10,18 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(15),
-      child: Text(title),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.title,
+      ),
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
             color.withOpacity(0.7),
             color,
           ],
-          begin:Alignment.topLeft,
-          end:Alignment.bottomRight,
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
         borderRadius: BorderRadius.circular(15),
       ),
